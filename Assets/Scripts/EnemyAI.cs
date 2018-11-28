65aa230da1a4fa1976c1f2b455f52a3ac056f3d7
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-11-28 12:01:37 -0500

    Soldiers Fight minions

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
-    [SerializeField] GameObject pathList;
+    GameObject pathList;
+    //Transform[] pathTargets;
     [SerializeField] List<Transform> pathTargets;
-    public Vector3 target;
+    Vector3 target;
     int index = -1;
-    public float health = 100;
+
+    [SerializeField] LayerMask enemyLayer;
+    public GameObject duelUnit = null;
+    bool attacking = false;
+    public int health = 100;
     public float speed = 5;
+    public int damage = 5;
+    public float attackTimer = 5;
+    float timer = 1;
+    float sightRange = 3;
 
     bool ignoreTarget = false;
     bool attackTower = false;
     bool dodgeArrows = false;
 
-    bool attacking = false;
-    bool freeWalk = false;
-    [SerializeField] LayerMask enemyLayer;
-    GameObject duelUnit;
-
     Rigidbody rig;
 
-	void Start ()
+	void Awake ()
     {
         rig = GetComponent<Rigidbody>();
+        pathList = GameObject.Find("PathList");
+        //pathTargets = pathList.GetComponentsInChildren<Transform>();
 		foreach(Transform t in pathList.GetComponentsInChildren<Transform>())
         {
             pathTargets.Add(t);
@@ -35,46 +41,95 @@ public class EnemyAI : MonoBehaviour {
 
     private void Update()
     {
-        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 3, Vector3.zero, enemyLayer);
-        foreach (RaycastHit hit in hits)
+        if(!duelUnit)
         {
-            if (hit.transform.gameObject.layer == enemyLayer && !freeWalk && !attacking)
+            attacking = false;
+            Collider[] enemies = Physics.OverlapSphere(transform.position, sightRange, enemyLayer);
+            foreach (Collider en in enemies)
             {
-                attacking = true;
-                duelUnit = hit.transform.gameObject;
-                //hit.transform.gameObject.GetComponent<>
+                if (!duelUnit && en.gameObject.GetComponent<SoldierAI>())
+                {
+                    attacking = true;
+                    duelUnit = en.gameObject;
+                }
             }
         }
-        if(attacking && duelUnit != null && !freeWalk)
+        if (attacking && duelUnit)
         {
             Combat();
         }
+
     }
 
     void FixedUpdate ()
     {
-        if (Vector3.Distance(transform.position, target) < 2 && index < pathTargets.Count && !attacking) NextTarget();
-        if (index <= pathTargets.Count - 1 && !attacking) rig.AddForce((transform.position - target).normalized * -speed * 100);
+        if (index <= pathTargets.Count && !attacking)
+        {
+            if (Vector3.Distance(transform.position, target) > .5f && index != 12)
+            {
+                rig.AddForce((transform.position - target).normalized * -speed * 100);
+            }
+        }
+
+        if (Vector3.Distance(transform.position, target) < 2 && index < pathTargets.Count - 1 && !attacking)
+            NextTarget();
+
         rig.velocity = Vector3.zero;
 	}
 
     void NextTarget()
     {
         index++;
-        freeWalk = false;
-        target = pathTargets[index].position;
+        
         if(index == 11)
         {
             target.x += Random.Range(-20f, 20f);
-            target.z += Random.Range(-10f, 10f);
-            attacking = true;
+            target.z += Random.Range(-5f, 5f);
+            attacking = false;
+        }
+        else
+        {
+            target = pathTargets[index].position;
         }
         target.x += Random.Range(-2f, 2f);
         target.z += Random.Range(-2f, 2f);
+        sightRange = 10f;
     }
 
     void Combat()
     {
-        
+        if (health > 0)
+        {
+            timer -= Time.deltaTime;
+            if (duelUnit)
+            {
+                if (Vector3.Distance(transform.position, duelUnit.transform.position) > 2)
+                {
+                    transform.Translate((transform.position - duelUnit.transform.position).normalized * -speed * Time.deltaTime);
+                    rig.velocity = Vector3.zero;
+                }
+                else if (timer <= 0)
+                {
+                    timer = attackTimer;
+                    duelUnit.GetComponent<SoldierAI>().health -= damage;
+                    if(duelUnit.GetComponent<SoldierAI>().health < 0)
+                    {
+                        attacking = false;
+                        Destroy(duelUnit);
+                        duelUnit = null;
+                    }
+                }
+            }
+            if(!duelUnit)
+            {
+                attacking = false;
+                duelUnit = null;
+            }
+        }
+        else
+        {
+            duelUnit.GetComponent<SoldierAI>().NextTarget();
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SoldierAI.cs
+++ Assets/Scripts/SoldierAI.cs
@@ -2,15 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoldierAI : MonoBehaviour {
+public class SoldierAI : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] LayerMask enemyLayer;
+    GameObject duelUnit = null;
+
+    public int health = 100;
+    public float speed = 2;
+    public int damage = 2;
+    public float attackTimer = 5;
+    float timer = 0;
+
+    Rigidbody rig;
+    Vector3 spawn;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        spawn = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!duelUnit)
+        {
+            NextTarget();
+        }
+        if (health > 0)
+        {
+            timer -= Time.deltaTime;
+            if (duelUnit)
+            {
+                if (Vector3.Distance(transform.position, duelUnit.transform.position) > 2)
+                {
+                    transform.Translate((transform.position - duelUnit.transform.position).normalized * -speed * Time.deltaTime);
+                    rig.velocity = Vector3.zero;
+                }
+                else if (timer <= 0)
+                {
+                    timer = attackTimer;
+                    duelUnit.GetComponent<EnemyAI>().health -= damage;
+                    if (duelUnit.GetComponent<EnemyAI>().health < 0)
+                    {
+                        Destroy(duelUnit);
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            duelUnit.GetComponent<EnemyAI>().duelUnit = null;
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!duelUnit && Vector3.Distance(transform.position, spawn) > .25f)
+        {
+            rig.AddForce((transform.position - spawn).normalized * -speed * 100);
+        }
+        rig.velocity = Vector3.zero;
+    }
+
+    public void NextTarget()
+    {
+        Collider[] enemies = Physics.OverlapSphere(transform.position, 3f, enemyLayer);
+        foreach(Collider en in enemies)
+        {
+            if(!duelUnit)
+            {
+                duelUnit = en.gameObject;
+            }
+        }
+    }
 }
