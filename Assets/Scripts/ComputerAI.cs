760636226c402ba2b08d4266c58c6acfa1f7ec1d
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-11-29 13:45:36 -0500

    Started AI
    Computer and Manager work
    computer needs to upgrade towers

---
+++ Assets/Scripts/ComputerAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerAI : MonoBehaviour
+{
+    class Tower
+    {
+        public Transform location;
+        public string type;
+        public int level;
+
+        public Tower(Transform plotSpot, string t, int lvl)
+        {
+            location = plotSpot;
+            type = t;
+            level = lvl;
+        }
+    }
+
+    TurnManager bigBoss;
+    [SerializeField] List<Tower> towers = new List<Tower>();
+    [SerializeField] GameObject towerSpotList;
+    [SerializeField] List<Transform> towerPlots = new List<Transform>();
+    public int economicPoints = 0;
+
+    [SerializeField] GameObject[] TowerTypes;
+    int i = 1;
+
+
+    void Start ()
+    {
+        bigBoss = GameObject.Find("Manager").GetComponent<TurnManager>();
+        economicPoints += 30;
+        foreach(Transform t in towerSpotList.GetComponentsInChildren<Transform>())
+        {
+            towerPlots.Add(t);
+        }
+        towerPlots.RemoveAt(0);
+
+	}
+
+	void Update ()
+    {
+        if(economicPoints == 40)
+        {
+            Debug.Log(economicPoints);
+        }
+
+		if(bigBoss.roundOver)
+        {
+            if(economicPoints >= 10 && towers.Count < towerPlots.Count)
+            {
+                
+                economicPoints -= 10;
+                Debug.Log(economicPoints);
+                BuyTower();
+            }
+        }
+	}
+
+    void BuyTower()
+    {
+        int spot = towerPlots.Count - i;
+        int typeTow = Random.Range(0, TowerTypes.Length);
+
+        GameObject tow = Instantiate(TowerTypes[typeTow], towerPlots[spot].position, towerPlots[spot].rotation) as GameObject;
+        tow.name = TowerTypes[typeTow].name + " lvl:1";
+        Tower addME = new Tower(towerPlots[spot], TowerTypes[typeTow].name, 1);
+        towers.Add(addME);
+        i++;
+
+    }
+    void UpgradeTower()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour {
+
+    ComputerAI comp;
+
+    public int difficulty = 0;
+    public int currentRound = 1;
+    float timer = 2f;
+
+    [SerializeField] Transform enemySpawn;
+    EnemyAI[] liveEnemies;
+    GameObject[] minionsMade; 
+    int enemyTypes = 0;
+    float enemySpawnTime = 1f;
+
+    bool ready = false;
+    public bool roundOver = false;
+    public bool roundRunning = false;
+
+
+	void Start ()
+    {
+        comp = GameObject.Find("Computer").GetComponent<ComputerAI>();
+	}
+
+
+	void Update ()
+    {
+        liveEnemies = GameObject.FindObjectsOfType<EnemyAI>();
+        if(liveEnemies.Length <= enemyTypes && roundRunning)
+        {
+            roundRunning = false;
+            roundOver = true;
+            comp.economicPoints += 10 + (difficulty * 10);
+            //player gets points to spend based on current round and difficulty
+        }
+        if (roundOver)
+        {
+            
+            //Player UI for adding too minions appear
+            //Player UI for choosing role, if final wave ask if ready
+            //
+            
+        }
+        else
+        {
+            //hide ui and save changes
+        }
+
+        if (ready)
+        {
+            //minionsMade = CREATOR minions array
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                roundRunning = true;
+                roundOver = false;
+                ready = false;
+                currentRound++;
+                timer = 2f;
+                enemySpawnTime = .5f;
+            }
+        }
+
+        if(roundRunning)
+        {
+            enemySpawnTime -= Time.deltaTime;
+            if (enemySpawnTime <= 0)
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    foreach (GameObject gm in minionsMade)
+                    {
+                        Instantiate(gm, enemySpawn.position, Quaternion.identity);
+                    }
+                }
+                enemySpawnTime = 3f;
+            }
+        }
+
+    }
+}
