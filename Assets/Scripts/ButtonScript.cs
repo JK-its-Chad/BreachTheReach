3d3b01deb92885e351ac2380a0f5b7ba0603e50f
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-12-06 11:12:02 -0500

    my button system

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    [SerializeField] TurnManager bigBoss;
+    [SerializeField] UpgradeMinions minMan;
+    public string func = "";
+
+    public void Click()
+    {
+        if(bigBoss && (func == "ReadyUp" || 
+            func == "playSup" || func == "playAtck" || 
+            func == "diffUP" || func == "diffDOWN" || 
+            func == "groupUP" || func == "groupDOWN"))
+        {
+            bigBoss.Invoke(func, 0);
+        }
+        else
+        {
+            Debug.Log(bigBoss.name);
+            Debug.Log(func);
+        }
+
+        if(minMan && (func == "hpUP" || func == "hpDown" || 
+            func == "dmgUP" || func == "dmgDown" || 
+            func == "spdUP" || func == "spdDown" ||
+            func == "atckspdUP" || func == "atckspdDown" ||
+            func == "SwapDodge" || func == "SwapIgnore" || func == "SwapTower"))
+        {
+            minMan.Invoke(func, 0);
+        }
+        else
+        {
+            Debug.Log(minMan.name);
+            Debug.Log(func);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/UIinteract.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIinteract : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+    public string buttonName;
+
+    [SerializeField] Transform start;
+    [SerializeField] Transform end;
+
+    LineRenderer line;
+    public LayerMask button;
+
+    Quaternion lastRotation;
+    Quaternion currentRotation;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        line = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis(buttonName) >= 0.9)
+        {
+            line.SetPosition(0, start.position);
+            line.SetPosition(1, end.position);
+            RaycastHit hit;
+            if(Physics.Raycast(start.position, start.forward, out hit, 20, button))
+            {
+                if (hit.collider.gameObject.GetComponent<ButtonScript>())
+                {
+                    hit.collider.gameObject.GetComponent<ButtonScript>().Click();
+                }
+            }
+        }
+        if (Input.GetAxis(buttonName) < 0.9)
+        {
+            line.SetPosition(1, start.position);
+        }
+    }
+
+
+}
